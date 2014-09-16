9efcf24ba521bc62c431fbbf5df82505be1fdd23
Azaezel <quillus@hotmail.com>
2014-09-15 21:13:45 -0500

    Order of operations

--- Templates/Full/game/shaders/common/lighting/advanced/deferredShadingP.hlsl
+++ Templates/Full/game/shaders/common/lighting/advanced/deferredShadingP.hlsl
@@ -42,8 +42,8 @@ float4 main( PFXVertToPix IN,
 	colorBuffer *= (1.0 - colorBuffer.a);
    }
 
-   colorBuffer *= float4(lightBuffer.rgb, 1.0);
    colorBuffer += float4(specular, specular, specular, 1.0);
+   colorBuffer *= float4(lightBuffer.rgb, 1.0);
 
    return hdrEncode( colorBuffer );   
 }
