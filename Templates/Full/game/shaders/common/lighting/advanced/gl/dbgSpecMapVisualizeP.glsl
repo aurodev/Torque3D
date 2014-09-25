c31c12119b2e108cf592631ff004e200f287b00e
Azaezel <quillus@hotmail.com>
2014-09-25 01:50:34 -0500

    specular map visualization correction. parallax implicit to explicit conversion

--- Templates/Full/game/shaders/common/lighting/advanced/gl/dbgSpecMapVisualizeP.glsl
+++ Templates/Full/game/shaders/common/lighting/advanced/gl/dbgSpecMapVisualizeP.glsl
@@ -19,8 +19,8 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 // IN THE SOFTWARE.
 //-----------------------------------------------------------------------------
-#include "shadergen:/autogenConditioners.h"
 #include "../../../gl/hlslCompat.glsl"
+#include "shadergen:/autogenConditioners.h"
 #include "../../../postfx/gl/postFx.glsl"
 
 uniform sampler2D colorBufferTex;
